2ef1fac6f400b6765dabd0b70ec7821636e46c7e
Sam Hart <s.w.hart22@gmail.com>
2017-03-13 15:14:19 -0500

    first commit

---
+++ js/graphic.js
@@ -0,0 +1,106 @@
+// Global vars
+var pymChild = null;
+var isMobile = false;
+
+/*
+ * Initialize the graphic.
+ */
+var onWindowLoaded = function() {
+    if (Modernizr.svg) {
+        pymChild = new pym.Child({
+            renderCallback: render
+        });
+    } else {
+        pymChild = new pym.Child({});
+    }
+
+    pymChild.onMessage('on-screen', function(bucket) {
+        ANALYTICS.trackEvent('on-screen', bucket);
+    });
+    pymChild.onMessage('scroll-depth', function(data) {
+        data = JSON.parse(data);
+        ANALYTICS.trackEvent('scroll-depth', data.percent, data.seconds);
+    });
+}
+
+/*
+ * Render the graphic.
+ */
+var render = function(containerWidth) {
+    if (!containerWidth) {
+        containerWidth = DEFAULT_WIDTH;
+    }
+
+    if (containerWidth <= MOBILE_THRESHOLD) {
+        isMobile = true;
+    } else {
+        isMobile = false;
+    }
+
+    //Render the chart!
+    renderGraphic({
+         container: '#graphic',
+         width: containerWidth,
+         data: []
+     });
+
+    // Update iframe
+    if (pymChild) {
+        pymChild.sendHeight();
+    }
+}
+
+/*
+ * Render a graphic.
+ */
+var renderGraphic = function(config) {
+    var aspectWidth = 4;
+    var aspectHeight = 3;
+
+    var margins = {
+        top: 0,
+        right: 15,
+        bottom: 20,
+        left: 15
+    };
+
+    // Calculate actual chart dimensions
+    var chartWidth = config['width'] - margins['left'] - margins['right'];
+    var chartHeight = Math.ceil((config['width'] * aspectHeight) / aspectWidth) - margins['top'] - margins['bottom'];
+
+    // Clear existing graphic (for redraw)
+    var containerElement = d3.select(config['container']);
+    containerElement.html('');
+
+    // Create container
+    var chartElement = containerElement.append('svg')
+        .attr('width', chartWidth + margins['left'] + margins['right'])
+        .attr('height', chartHeight + margins['top'] + margins['bottom'])
+        .append('g')
+        .attr('transform', 'translate(' + margins['left'] + ',' + margins['top'] + ')');
+
+    // Draw here!
+    
+	var circle = chartElement.append('circle')
+		.attr('cx',50)
+		.attr('cy',50)
+		.attr('r',25)
+		.attr('fill','red');
+	
+	var circle2 = chartElement.append('circle')
+		.attr('cx',350)
+		.attr('cy',250)
+		.attr('r',50)
+		.attr('fill','blue');
+    
+}
+
+
+
+
+
+/*
+ * Initially load the graphic
+ * (NB: Use window.load to ensure all images have loaded)
+ */
+window.onload = onWindowLoaded;
